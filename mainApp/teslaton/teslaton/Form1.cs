1dc5589a49d3104b0a6ed16df05e2c2cbb959558
Zoomim <zoomim@users.noreply.github.com>
2016-12-03 03:46:31 +0100

    Add files via upload

---
+++ mainApp/teslaton/teslaton/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace teslaton
+{
+
+    public partial class Form1 : Form
+    {
+        string connection_string = @"Server=tcp:uzubase.database.windows.net,1433;Initial Catalog=Dbase;Persist Security Info=False;User ID=uzuyami;Password=HoSEk258;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public DataTable tabulka;
+
+    public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabulka = GetResultsTable();
+            dataGridView1.DataSource = tabulka;
+            nacti_data();
+        }
+
+        private void nacti_data()
+        {
+            SqlConnection connection = new SqlConnection(connection_string);
+            connection.Open();
+
+            if (connection.State == System.Data.ConnectionState.Open)
+                label1.Text = "Connection Succeeded!";
+            else
+                label1.Text = "Connection Failure!";
+
+
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand("select * from dbo.popelnice",
+                                                     connection);
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                tabulka.Rows.Add(myReader[0].ToString(), myReader[1].ToString(), myReader[2].ToString(), myReader[3].ToString(), myReader[4].ToString(), myReader[5].ToString());
+            }
+
+
+        }
+
+        public DataTable GetResultsTable()
+        {
+            // Here we create a DataTable with four columns.
+            DataTable table = new DataTable();
+            table.Columns.Add("ID", typeof(string));
+            table.Columns.Add("Naplňenost", typeof(string));
+            table.Columns.Add("Převrácení", typeof(string));
+            table.Columns.Add("Požár", typeof(string));
+            table.Columns.Add("Lokace šířka", typeof(string));
+            table.Columns.Add("Lokace délka", typeof(string));
+            return table;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 urlform = new Form2();
+            urlform.Show();
+        }
+    }
+}
---
+++ mainApp/teslaton/teslaton/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teslaton
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.google.cz/maps/place/49%C2%B012'53.2%22N+16%C2%B033'40.2%22E/@49.2147755,16.5589843,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d49.214772!4d16.561173");
+        }
+    }
+}
